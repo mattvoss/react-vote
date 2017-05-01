ab4420a879e8766502561d1e7f43e388f7990b29
Matthew Voss <voss.matthew@gmail.com>
2017-05-01 13:39:16 -0500

    Remove ignored files

--- config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  //checkinServerUrl: 'http://0.0.0.0:3001',
-  checkinServerUrl: 'https://checkin.regionvivpp.org',
-  start: '2017-04-28 09:00:00',
-  end: '2017-04-29 07:00:00',
-};
